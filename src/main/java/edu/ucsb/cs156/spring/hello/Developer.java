6b08fce45c708264daaf28cc38d114309ad0ff4b
Amber Wang <ywang116@ucsb.edu>
2024-10-14 23:27:07 -0700

    updated with actual developer name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -20,7 +20,7 @@ public class Developer {
         // You may use just the name that is used on <https://bit.ly/cs156-f24-teams>
         // i.e. your first name, or your first and initial of last name
 
-        return "Chris G.";
+        return "Amber W.";
     }
 
     /**
@@ -30,7 +30,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "yw2398";
     }
 
     /**
@@ -40,13 +40,13 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("f24-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("f24-08");
+        team.addMember("Amber");
+        team.addMember("Caleb");
+        team.addMember("Christy");
+        team.addMember("Divyani");
+        team.addMember("Mike");
+        team.addMember("Oscar");
         return team;
     }
 }
